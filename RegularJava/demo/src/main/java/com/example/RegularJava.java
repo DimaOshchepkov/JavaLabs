729152c9005df34cf7ab80f58272588ba6747904
DmitiyOshchepkov <odo.2003@yandex.ru>
2023-09-17 20:30:14 +0300

    add testIsIPAdress

---
+++ RegularJava/demo/src/main/java/com/example/RegularJava.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import java.util.regex.Pattern;
+
+public class RegularJava {
+    
+    public boolean isIPAddress() {
+        
+    }
+}
---
+++ RegularJava/demo/src/test/java/RegularJavaUnitsTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.example.RegularJava;
+
+public class RegularJavaUnitsTest {
+    RegularJava rj = new RegularJava();
+
+    @Test
+    public void testIsIPAddress() {
+        String trueIp = "255.255.255.255";
+        Assert.assertEquals(trueIp + " is correct", true, rj.isIPAddress());
+    }
+}
